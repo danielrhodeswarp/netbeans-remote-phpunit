3fe133123f93b6a5c8d85f07f17eb691b22a2ed8
Daniel Rhodes <daniel.rhodes@warpasylum.co.uk>
2013-07-08 22:21:02 +0100

    made suitable for Vagrant AND added rerun failed support

--- README.md
+++ README.md
@@ -8,7 +8,11 @@ like fentie's code this allows NetBeans to
 work with remote PHPUnit execution.
 
 But this is for a "remote" server of Vagrant
-running on yuor localhost.
+running on your localhost.
 
+A bit of a mess at the moment but supports
+NetBeans "(re)run [all] tests" and "rerun failed" test buttons.
 
+Does NOT use the master PHPUnit test suite as bundled with NetBeans.
 
+You'll probably need the appropriate phpunit.xml in $REMOTE_PATH_TO_TESTS
--- phpunit-remote.sh
+++ phpunit-remote.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
+
 #echo $@ #uncomment to debug
+#echo $1 $2 $3 $4
+#echo $3 $4
+#in case of "Rerun failed" $3 will be '--filter' and $4 will be a massive filter string
+#$1 and $2 important for logging to an XML file (which local NetBeans then needs to
+#display results etc)
 
-REMOTE_SERVER=user@virtualhost
-REMOTE_PATH_TO_TESTS='/path/to/tests'
+REMOTE_SERVER=vagrant@localhost
+REMOTE_PATH_TO_TESTS='/var/www/html/your_website_in_vagrant/tests'
 
-# The very last argument is the path to the test(s) you want run
-export TEST=$9
+#Connect to your vagrant VM, cd to your test location and run phpunit with appropriate args
+if [ $3 = "--filter" ]
+then
+	#"rerun failed"
+	vagrant ssh -c "cd $REMOTE_PATH_TO_TESTS; phpunit $1 $2 --filter \"$4\""
+else
+	#"(re)run [all] tests"
+	vagrant ssh -c "cd $REMOTE_PATH_TO_TESTS; phpunit $1 $2"
+fi
 
-# Chop off everything up to "tests" in that path. "tests" is just an example path
-TEST=${TEST##r*tests}
-
-# Connect to your VM, cd to your test location and run phpunit with most of the args
-ssh $REMOTE_SERVER "cd $REMOTE_PATH_TO_TESTS; phpunit $1 $2 $3 $REMOTE_PATH_TO_TESTS/bootstrap.php $REMOTE_PATH_TO_TESTS/$TEST"
-
-# Copy the test output back to your local machine, where NetBeans expects to find it
-scp $REMOTE_SERVER:$2 $2
+#Copy the test output back to your local machine, where NetBeans expects to find it
+#might not work on mac. definitely won't work on win!
+scp -i ~/.vagrant.d/insecure_private_key -P 2222 $REMOTE_SERVER:$2 $2
